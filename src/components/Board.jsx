aa0ed9cc78a2237081e66555b9d15123143968b8
Md. Mahade Hassan <mahade.hassan.bd@gmail.com>
2024-03-06 18:06:13 +0600

    set squre value X

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,28 +1,36 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
-function Squre({ value }) {  
+function Squre() {  
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
   
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg" onClick={handleClick}>{value}</button>
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg" onClick={handleClick}>{ value}</button>
   )
 }
 
 export default function Board() {
+  const [squres, setSqures] = useState(Array(9).fill(null));
   return (
       <>
-          <div>
-            <Squre value='1'/>
-            <Squre value='2'/>
-            <Squre value='3'/>
+          <div className="flex">
+            <Squre />
+            <Squre />
+            <Squre />
           </div>
-          <div>
-            <Squre value='4'/>
-            <Squre value='5'/>
-            <Squre value='6'/>
+          <div className="flex">
+            <Squre />
+            <Squre />
+            <Squre />
           </div>
-          <div>
-           <Squre value='7'/>
-           <Squre value='8'/>
-           <Squre value='9'/>
+          <div className="flex">
+           <Squre />
+           <Squre />
+           <Squre />
           </div>   
       
       
